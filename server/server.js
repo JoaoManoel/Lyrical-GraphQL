786082dfd4d5a1d6e69f6af34240461fa39c738b
Joao Manoel Lins <joaomanoellins@gmail.com>
2017-08-02 23:47:13 -0300

    ...

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://root:root@ds129013.mlab.com:29013/lyricaldb2323';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
