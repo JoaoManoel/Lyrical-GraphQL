c30356ee19493cfb75f712e513c1bd405da6fae8
Joao Manoel Lins <joaomanoellins@gmail.com>
2017-08-04 22:44:00 -0300

    section 10 done

---
+++ client/components/LyricList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class LyricList extends Component {
+  onLike(id) {
+    console.log(id);
+  }
+  
+  renderList() {
+    return this.props.lyrics.map(({ id, content}) => {
+      return (
+        <li key={id} className="collection-item">
+          {content}
+          <i className="material-icons" onClick={ () => this.onLike(id) }>thumb_up</i>
+        </li>
+      );
+    });
+  }
+  render() {
+    return (
+      <ul className="collection">{this.renderList()}</ul>
+    )
+  }
+}
+
+export default LyricList;
